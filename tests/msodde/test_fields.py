677d2d4fbe51b472fc2aa26f8f666b00ccd250f3
Samir Aguiar <samirjaguiar@gmail.com>
2018-02-26 09:24:07 -0300

    msodde: add more unit tests for documents
    Since we have different kind of supported formats,
    unit tests for all of the documents formats were
    added. The documents contain not only DDE links,
    but also pictures, text and so on. These, however,
    are ignored for older formats since only links
    are supported.

--- tests/msodde/test_fields.py
+++ tests/msodde/test_fields.py
@@ -6,18 +6,29 @@ with multiple blacklisted fields
 """
 
 import unittest
+import re
 from oletools import msodde
 from tests.test_utils import OutputCapture, DATA_BASE_DIR as BASE_DIR
 from os import listdir, path
 
-EXPECTED_FIELDS = [
-    'DDEAUTO c:\\\\windows\\\\system32\\\\cmd.exe "/k calc.exe \'\'""\'"\'"\'"\'"\'"',
-    u'DDE c:\\\\windows\\\\system32\\\\cmd.exe "ЁЂ🙇التَّطْبِي؀؁؂؃؄؅؜۝܏᠎''',
-    'IMPORT  "C:\\\\Users\\\\foo\\\\Desktop\\\\cat.jpg" \* MERGEFORMAT',
-    'INCLUDEPICTURE  "C:\\\\Users\\\\foo\\\\Desktop\\\\cat.jpg" \* MERGEFORMATINET',
-    'INCLUDETEXT  "C:\\\\Users\\\\foo\\\\Desktop\\\\doc.txt" \* MERGEFORMAT',
-    'INCLUDE  "C:\\\\Users\\\\foo\\\\Desktop\\\\doc.txt" \* MERGEFORMAT'
-]
+EXPECTED_FIELDS_LEGACY = {
+    re.compile(r'DDEAUTO'): ['cmd.exe', 'calc.exe'],
+    # use word boundaries so DDE is not confused with DDEAUTO
+    re.compile(r'\bDDE\b'): ['cmd.exe']
+}
+
+EXPECTED_FIELDS = dict(EXPECTED_FIELDS_LEGACY.items() + {
+    re.compile(r'IMPORT'): ['cat.jpg'],
+    re.compile(r'INCLUDETEXT'): ['doc.txt'],
+    re.compile(r'\bINCLUDE\b'): ['doc.txt']
+}.items())
+
+# from https://stackoverflow.com/a/93029/2883579
+CONTROL_CHARS = ''.join(map(unichr, range(0, 32) + range(127, 160)))
+CONTROL_CHARS_REGEX = re.compile('[%s]' % re.escape(CONTROL_CHARS))
+
+LEGACY_FILES_EXT = ['.doc', '.dot']
+
 
 class TestWordDocuments(unittest.TestCase):
     test_data_dir = path.join(BASE_DIR, 'msodde', 'fields')
@@ -27,19 +38,46 @@ class TestWordDocuments(unittest.TestCase):
         files = listdir(files_path)
 
         for filename in files:
+            file_extension = path.splitext(filename)[1]
+            is_legacy_file = file_extension in LEGACY_FILES_EXT
+
             with OutputCapture() as capturer:
                 msodde.main([path.join(files_path, filename), ])
-                self.assertTrue(self.all_fields_were_found(capturer))
+                self.assertTrue(
+                    self._all_fields_were_found(capturer, is_legacy_file))
 
-    def all_fields_were_found(self, capturer):
-        fields_to_look = EXPECTED_FIELDS
+    @staticmethod
+    def _all_fields_were_found(capturer, legacy_file=False):
+        # not all fields are shown for legacy Office files
+        if legacy_file:
+            fields_to_look = EXPECTED_FIELDS_LEGACY
+        else:
+            fields_to_look = EXPECTED_FIELDS
 
+        dde_links_started = False
         for line in capturer:
-            if not line.strip():
+            # remove non-printable characters (for Office 97-2003 files)
+            line = CONTROL_CHARS_REGEX.sub('', line)
+
+            if 'DDE Links:' in line:
+                dde_links_started = True
                 continue
 
-            fields_to_look = [f for f in fields_to_look if f not in line]
-        
+            if not line.strip() or not dde_links_started:
+                continue
+
+            # remove from dict if the field was found and content matches
+            for key in fields_to_look:
+                if not key.search(line):
+                    continue
+
+                value = fields_to_look[key]
+
+                if all(field_content in line for field_content in value):
+                    fields_to_look.pop(key, None)
+                    break
+
+        # if everything was found `fields_to_look` should be empty
         return not fields_to_look
 
 
